8b9b89d7bea57209093a554ef77d65bcfe72b750
steve124090383-droid <steve124090383@gmail.com>
2025-12-02 13:59:26 +0800

    Update fourBottums1.cpp

--- src/fourBottums1.cpp
+++ src/fourBottums1.cpp
@@ -17,27 +17,27 @@ float fricVal=0.1f;
 float speedVal=1.0f;
 
 
-int inside(int mx,int my,int x,int y,int w,int h){
+int inside(int mx,int my,int x,int y,int w,int h) {
     if(mx>=x && mx<=x+w && my>=y && my<=y+h) return 1;
     return 0;
 }
 
 //把屏幕保存到栈里面
-void saveScr(){
+void saveScr() {
     IMAGE img;
     getimage(&img,0,0,getwidth(),getheight());
     scrS.push(img);
 }
 
 //从栈里面恢复屏幕
-void restoreScr(){
+void restoreScr() {
     if(scrS.empty()) return;
     putimage(0,0,&scrS.top());
     scrS.pop();
 }
 
 //初始化
-void initBtns(int w,int h){
+void initBtns(int w,int h) {
     btnW=140; BtnH=40;
     btnx=w-btnW-15;
     int baseY=h-290;
@@ -48,7 +48,7 @@ void initBtns(int w,int h){
 }
 
 //把按钮画出来
-void drawBtns(){
+void drawBtns() {
     setfillcolor(RGB(210,210,210));
     solidrectangle(btnx,colrBtnY,btnx+btnW,colrBtnY+BtnH);
     solidrectangle(btnx,gravBtnY,btnx+btnW,gravBtnY+BtnH);
@@ -62,7 +62,7 @@ void drawBtns(){
 }
 
 //用来选择颜色
-void colorSelect(){
+void colorSelect() {
     saveScr();
     const char* colors[4]={"Red","Blue","Yellow","Orange"};
     int cx=getwidth()/2-80;
@@ -71,11 +71,19 @@ void colorSelect(){
     outtextxy(cx,cy-40,"Pick Color (ESC cancel)");
     
     //本来是一个个单独绘制，重复了，后来优化成循环来绘制
-    for(int i=0;i<4;i++){
-        if(strcmp(colors[i],"Red")==0) setfillcolor(RED);
-        else if(strcmp(colors[i],"Blue")==0) setfillcolor(BLUE);
-        else if(strcmp(colors[i],"Yellow")==0) setfillcolor(YELLOW);
-        else setfillcolor(RGB(255,165,0));
+    for(int i=0;i<4;i++) {
+        if(strcmp(colors[i],"Red")==0) {
+            setfillcolor(RED);
+        }
+        else if(strcmp(colors[i],"Blue")==0) {
+            setfillcolor(BLUE);
+        }
+        else if(strcmp(colors[i],"Yellow")==0) {
+            setfillcolor(YELLOW);
+        }
+        else {
+            setfillcolor(RGB(255,165,0));
+        }
 
         solidrectangle(cx,cy+i*48,cx+150,cy+i*48+42);
         setcolor(BLACK);
@@ -88,19 +96,23 @@ void colorSelect(){
     outtextxy(cx+40,cy+222,"Cancel");
 
     bool done=false;
-    while(!done){
+    while(!done) {
         ExMessage m;
-        if(peekmessage(&m,EM_MOUSE|EM_KEY)){
-            if(m.message==WM_KEYDOWN && m.vkcode==VK_ESCAPE) break;
-            if(m.message==WM_LBUTTONDOWN){
-                for(int i=0;i<4;i++){
-                    if(inside(m.x,m.y,cx,cy+i*48,150,42)){
+        if(peekmessage(&m,EM_MOUSE|EM_KEY)) {
+            if(m.message==WM_KEYDOWN && m.vkcode==VK_ESCAPE) {
+                break;
+            }
+            if(m.message==WM_LBUTTONDOWN) {
+                for(int i=0;i<4;i++) {
+                    if(inside(m.x,m.y,cx,cy+i*48,150,42)) {
                         strcpy(colorN,colors[i]);
                         done=true;
                         break;
                     }
                 }
-                if(inside(m.x,m.y,cx,cy+210,150,40)) break;
+                if(inside(m.x,m.y,cx,cy+210,150,40)) {
+                    break;
+                }
             }
         }
         Sleep(16); 
@@ -109,7 +121,7 @@ void colorSelect(){
 }
 
 //用来输入数字
-void numInput(const char* t,float* val){
+void numInput(const char* t,float* val) {
     saveScr();
     char buf[40]="";
     //加一个缓冲区，储存
@@ -118,7 +130,7 @@ void numInput(const char* t,float* val){
     const int cy = 260;
     
     bool finish=false;
-    while(!finish){
+    while(!finish) {
         cleardevice();
         outtextxy(cx,cy-50,t);
         outtextxy(cx,cy-25,"(Enter=OK,ESC=Cancel)");
@@ -139,27 +151,35 @@ void numInput(const char* t,float* val){
         outtextxy(cx+150,cy+82,"Cancel");
 
         ExMessage m;
-        if(peekmessage(&m,EM_MOUSE|EM_KEY)){
-            if(m.message==WM_KEYDOWN){
-                if(m.vkcode==VK_RETURN && len>0){
+        if(peekmessage(&m,EM_MOUSE|EM_KEY)) {
+            if(m.message==WM_KEYDOWN) {
+                if(m.vkcode==VK_RETURN && len>0) {
                     *val=atof(buf);
                     finish=true;
-                }else if(m.vkcode==VK_ESCAPE) finish=true;
-                else if(m.vkcode==VK_BACK && len>0){
+                }
+                else if(m.vkcode==VK_ESCAPE) {
+                    finish=true;
+                }
+                else if(m.vkcode==VK_BACK && len>0) {
                     len--;
                     buf[len]='\0';
-                }else if((m.vkcode>='0' && m.vkcode<='9') || m.vkcode=='.'){
+                }
+                else if((m.vkcode>='0' && m.vkcode<='9') || m.vkcode=='.') {
                     if(len<39){
                         buf[len]=m.vkcode;
                         len++;
                         buf[len]='\0';
                     }
                 }
-            }else if(m.message==WM_LBUTTONDOWN){
-                if(inside(m.x,m.y,cx,cy+70,100,40) && len>0){
+            }
+            else if(m.message==WM_LBUTTONDOWN) {
+                if(inside(m.x,m.y,cx,cy+70,100,40) && len>0) {
                     *val=atof(buf);
                     finish=true;  //点击了OK键
-                }else if(inside(m.x,m.y,cx+130,cy+70,100,40)) finish=true;
+                }
+                else if(inside(m.x,m.y,cx+130,cy+70,100,40)) {
+                    finish=true;
+                }
             }
         }
     }
@@ -168,20 +188,39 @@ void numInput(const char* t,float* val){
 }
 
 //更新
-void updBtns(){
+void updBtns() {
     ExMessage m;
-    while(peekmessage(&m,EM_MOUSE)){
-        if(m.message==WM_LBUTTONDOWN){
-            if(inside(m.x,m.y,btnx,colrBtnY,btnW,BtnH)) colorSelect();
-            else if(inside(m.x,m.y,btnx,gravBtnY,btnW,BtnH)) numInput("Gravity",&gravVal);
-            else if(inside(m.x,m.y,btnx,fricBtnY,btnW,BtnH)) numInput("Friction",&fricVal);
-            else if(inside(m.x,m.y,btnx,spdBtnY,btnW,BtnH)) numInput("Speed",&speedVal);
+    while(peekmessage(&m,EM_MOUSE)) {
+        if(m.message==WM_LBUTTONDOWN) {
+            if(inside(m.x,m.y,btnx,colrBtnY,btnW,BtnH)) {
+                colorSelect();
+            }
+            else if(inside(m.x,m.y,btnx,gravBtnY,btnW,BtnH)) {
+                numInput("Gravity",&gravVal);
+            }
+            else if(inside(m.x,m.y,btnx,fricBtnY,btnW,BtnH)) {
+                numInput("Friction",&fricVal);
+            }
+            else if(inside(m.x,m.y,btnx,spdBtnY,btnW,BtnH)) {
+                numInput("Speed",&speedVal);
+            }
         }
     }
 }
 
 //返回输入的值
-char* getColor(){return colorN;}
-float getGrav(){return gravVal;}
-float getFric(){return fricVal;}
-float getSpeedVal(){return speedVal;}
+char* getColor() {
+    return colorN;
+}
+
+float getGrav() {
+    return gravVal;
+}
+
+float getFric() {
+    return fricVal;
+}
+
+float getSpeedVal() {
+    return speedVal;
+}
